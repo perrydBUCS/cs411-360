217e3de1aeac5bd07e000789f8f3adfaba562c44
Perry Donham <perryd@bu.edu>
2017-12-09 19:08:56 -0500

    Thought this was deleted...

--- config/mongo.js
+++
@@ -1,5 +0,0 @@
-const mongoConfig = {
-    CONNECTION_STRING: 'mongodb://localhost/cs411-360'
-}
-module.exports = mongoConfig
-//todo Set up specific user just for this database (should always do that)
